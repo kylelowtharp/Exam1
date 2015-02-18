86ad2d60c1b08615606873497bd1761d58570b61
kylelowtharp <klv9700@saintjoe.edu>
2015-02-18 13:23:01 -0500

    list to the filter function#

---
+++ list.js
@@ -0,0 +1,8 @@
+
+
+var FIGHTERS_FILE = "[\n " + [
+'{"name": "Jon Jones", "class": "Light Heavyweight", "wins": 21, "losses" 1, "age": 27}',
+'{"name": "Fabricio Werdum", "class": "Heavyweight", "wins": 19, "losses": 5, "age": 37}',
+'{"name": "Cain Velasquez", "class": "Heavyweight", "wins": 13, "losses": 1, "age": 32}'
+
+].join(",\n ") + "\n];
